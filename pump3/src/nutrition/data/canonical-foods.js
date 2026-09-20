8c865d0c72f475f2236ae8d2e8ea52b271c6add3
Messineo <idoshirhen15@gmail.com>
2026-09-21 00:51:42 +0300

    Register verified nutrition batch v7

--- pump3/src/nutrition/data/canonical-foods.js
+++ pump3/src/nutrition/data/canonical-foods.js
@@ -4,6 +4,7 @@ import { OFFICIAL_FOODS_V3 } from './official-foods-v3.js';
 import { OFFICIAL_FOODS_V4 } from './official-foods-v4.js';
 import { OFFICIAL_FOODS_V5 } from './official-foods-v5.js';
 import { OFFICIAL_FOODS_V6 } from './official-foods-v6.js';
+import { OFFICIAL_FOODS_V7 } from './official-foods-v7.js';
 
 export const CANONICAL_FOODS = Object.freeze([
   ...OFFICIAL_FOODS_V1,
@@ -12,6 +13,7 @@ export const CANONICAL_FOODS = Object.freeze([
   ...OFFICIAL_FOODS_V4,
   ...OFFICIAL_FOODS_V5,
   ...OFFICIAL_FOODS_V6,
+  ...OFFICIAL_FOODS_V7,
 ]);
 
 export const CANONICAL_FOOD_BY_LEGACY_ALIAS = Object.freeze(Object.fromEntries(
