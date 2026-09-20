bf10a8886348a9b0e85ef52774eb20c1cf91ac17
Messineo <idoshirhen15@gmail.com>
2026-09-20 16:55:33 +0300

    Aggregate verified canonical foods

---
+++ pump3/src/nutrition/data/canonical-foods.js
@@ -0,0 +1,15 @@
+import { OFFICIAL_FOODS_V1 } from './official-foods-v1.js';
+import { OFFICIAL_FOODS_V2 } from './official-foods-v2.js';
+
+export const CANONICAL_FOODS = Object.freeze([
+  ...OFFICIAL_FOODS_V1,
+  ...OFFICIAL_FOODS_V2,
+]);
+
+export const CANONICAL_FOOD_BY_LEGACY_ALIAS = Object.freeze(Object.fromEntries(
+  CANONICAL_FOODS.flatMap((food) => [food.name, ...(food.aliases ?? [])].map((alias) => [alias, food.id])),
+));
+
+export const CANONICAL_FOOD_BY_ID = Object.freeze(Object.fromEntries(
+  CANONICAL_FOODS.map((food) => [food.id, food]),
+));
