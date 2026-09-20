0ed1d5119528349f219af1e6289b05d11dfd0cbb
Messineo <idoshirhen15@gmail.com>
2026-09-20 21:56:15 +0300

    nutrition: register USDA staple foods batch

--- pump3/src/nutrition/data/canonical-foods.js
+++ pump3/src/nutrition/data/canonical-foods.js
@@ -1,11 +1,13 @@
 import { OFFICIAL_FOODS_V1 } from './official-foods-v1.js';
 import { OFFICIAL_FOODS_V2 } from './official-foods-v2.js';
 import { OFFICIAL_FOODS_V3 } from './official-foods-v3.js';
+import { OFFICIAL_FOODS_V4 } from './official-foods-v4.js';
 
 export const CANONICAL_FOODS = Object.freeze([
   ...OFFICIAL_FOODS_V1,
   ...OFFICIAL_FOODS_V2,
   ...OFFICIAL_FOODS_V3,
+  ...OFFICIAL_FOODS_V4,
 ]);
 
 export const CANONICAL_FOOD_BY_LEGACY_ALIAS = Object.freeze(Object.fromEntries(
