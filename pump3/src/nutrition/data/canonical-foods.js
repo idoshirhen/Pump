991a61bbac3becd9dbbb2e15f1a2006a2653f327
Messineo <idoshirhen15@gmail.com>
2026-09-20 22:45:55 +0300

    nutrition: include verified staple batch v5

--- pump3/src/nutrition/data/canonical-foods.js
+++ pump3/src/nutrition/data/canonical-foods.js
@@ -2,12 +2,14 @@ import { OFFICIAL_FOODS_V1 } from './official-foods-v1.js';
 import { OFFICIAL_FOODS_V2 } from './official-foods-v2.js';
 import { OFFICIAL_FOODS_V3 } from './official-foods-v3.js';
 import { OFFICIAL_FOODS_V4 } from './official-foods-v4.js';
+import { OFFICIAL_FOODS_V5 } from './official-foods-v5.js';
 
 export const CANONICAL_FOODS = Object.freeze([
   ...OFFICIAL_FOODS_V1,
   ...OFFICIAL_FOODS_V2,
   ...OFFICIAL_FOODS_V3,
   ...OFFICIAL_FOODS_V4,
+  ...OFFICIAL_FOODS_V5,
 ]);
 
 export const CANONICAL_FOOD_BY_LEGACY_ALIAS = Object.freeze(Object.fromEntries(
