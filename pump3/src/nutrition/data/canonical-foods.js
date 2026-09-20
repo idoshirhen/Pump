3d2a92f57159ac5f1c1a44f41710cf563fe3b88e
Messineo <idoshirhen15@gmail.com>
2026-09-20 23:26:36 +0300

    Include verified USDA staples in canonical registry

--- pump3/src/nutrition/data/canonical-foods.js
+++ pump3/src/nutrition/data/canonical-foods.js
@@ -3,6 +3,7 @@ import { OFFICIAL_FOODS_V2 } from './official-foods-v2.js';
 import { OFFICIAL_FOODS_V3 } from './official-foods-v3.js';
 import { OFFICIAL_FOODS_V4 } from './official-foods-v4.js';
 import { OFFICIAL_FOODS_V5 } from './official-foods-v5.js';
+import { OFFICIAL_FOODS_V6 } from './official-foods-v6.js';
 
 export const CANONICAL_FOODS = Object.freeze([
   ...OFFICIAL_FOODS_V1,
@@ -10,6 +11,7 @@ export const CANONICAL_FOODS = Object.freeze([
   ...OFFICIAL_FOODS_V3,
   ...OFFICIAL_FOODS_V4,
   ...OFFICIAL_FOODS_V5,
+  ...OFFICIAL_FOODS_V6,
 ]);
 
 export const CANONICAL_FOOD_BY_LEGACY_ALIAS = Object.freeze(Object.fromEntries(
